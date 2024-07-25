876c0a5e247bd27b05a03bb1c045543df83a0ad1
Alex Riascos <160887203+X4Nn3R@users.noreply.github.com>
2024-07-25 03:58:26 +0000

    add random card generator Alex Riascos

--- src/app.js
+++ src/app.js
@@ -7,5 +7,53 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numeroAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numeroAleatorio];
+  }
+
+  let pinta = ["♦", "♥", "♠", "♣"];
+  let numero = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  function generarCarta() {
+    let pintaCarta = aleatorio(pinta);
+    let elementoPintaCarta = document.querySelector("#pintaCartaArriba");
+    elementoPintaCarta.innerHTML = pintaCarta;
+
+    let numeroCarta = aleatorio(numero);
+    let elementoNumeroCarta = document.querySelector("#numeroCarta");
+    elementoNumeroCarta.innerHTML = numeroCarta;
+    let elementoPintaCartaAbajo = document.querySelector("#pintaCartaAbajo");
+    elementoPintaCartaAbajo.innerHTML = pintaCarta;
+
+    elementoPintaCartaAbajo.style.transform = "rotate(180deg)";
+
+    if (pintaCarta == "♥" || pintaCarta == "♦") {
+      elementoPintaCartaAbajo.style.color = "red";
+      elementoPintaCarta.style.color = "red";
+      elementoNumeroCarta.style.color = "red";
+    } else {
+      elementoPintaCartaAbajo.style.color = "black";
+      elementoPintaCarta.style.color = "black";
+      elementoNumeroCarta.style.color = "black";
+    }
+  }
+
+  generarCarta();
+
+  let generarCartaBtn = document.querySelector("#generarCartaBtn");
+  generarCartaBtn.addEventListener("click", generarCarta);
 };
